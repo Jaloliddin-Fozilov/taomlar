fe8af2a6eae62c38e22861b9ff562eb7f9412775
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2021-11-17 21:56:10 +0500

    gridview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,41 @@ class Home extends StatelessWidget {
         centerTitle: true,
         title: Text("Ovqatlar Menyusi"),
       ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  child: Image.asset(
+                    "assets/images/cola.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  color: Colors.black.withOpacity(0.4),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "Ichimliklar",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
